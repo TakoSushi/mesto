afeeca00e7c944084a86020682f130c7a0c5404a
Petr Kuzora <petr1725@yandex.ru>
2023-01-11 23:46:24 +0300

    Проект готов к сдаче

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+let popup = document.querySelector('.popup'),
+    editButton = document.querySelector('.profile__edit-button'),
+    popupClose = document.querySelector('.popup__close'),
+    inputTitle = document.querySelector('.popup__input-title'),
+    inputProfession = document.querySelector('.popup__input-profession'),
+    profileName = document.querySelector('.profile__name'),
+    profileProfession = document.querySelector('.profile__profession'),
+    popupForm = document.querySelector('.popup__container');
+
+editButton.addEventListener('click', function() {
+  popup.classList.add('popup_opened');
+});
+
+popupClose.addEventListener('click', closePopupForm);
+
+popupForm.addEventListener('submit', handleFormSubmit);
+
+function closePopupForm() {
+  popup.classList.remove('popup_opened');
+  popupForm.reset();
+  inputTitle.placeholder = profileName.textContent;
+  inputProfession.placeholder = profileProfession.textContent;
+}
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+
+  if (inputTitle.value && inputProfession.value) {
+    profileName.textContent = inputTitle.value;
+    profileProfession.textContent = inputProfession.value;
+  } else if (inputTitle.value){
+    profileName.textContent = inputTitle.value;
+  } else if (inputProfession.value){
+    profileProfession.textContent = inputProfession.value;
+  }
+
+  closePopupForm();
+}
