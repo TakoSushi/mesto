f2856b229e041c9a1e9369287180186b9ab59d58
Petr Kuzora <petr1725@yandex.ru>
2023-01-14 17:07:25 +0300

    внесены изменения по замечаниям ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,20 @@
-let popup = document.querySelector('.popup'),
-    editButton = document.querySelector('.profile__edit-button'),
-    popupClose = document.querySelector('.popup__close'),
-    // inputTitle = document.querySelector('.popup__input-title'),
-    // inputProfession = document.querySelector('.popup__input-profession'),
-    profileName = document.querySelector('.profile__name'),
-    profileProfession = document.querySelector('.profile__profession'),
-    popupForm = document.querySelector('.popup__form');
+const popup = document.querySelector('.popup'),
+      editButton = document.querySelector('.profile__edit-button'),
+      popupClose = document.querySelector('.popup__close'),
+      inputTitle = document.querySelector('#input_user-name'),
+      inputProfession = document.querySelector('#input_user-profession'),
+      profileName = document.querySelector('.profile__name'),
+      profileProfession = document.querySelector('.profile__profession'),
+      popupForm = document.querySelector('.popup__form');
 
 editButton.addEventListener('click', function() {
+  inputTitle.value = profileName.textContent;
+  inputProfession.value = profileProfession.textContent;
   popup.classList.add('popup_opened');
 });
 
-popupClose.addEventListener('click', closePopupForm);
-
-popupForm.addEventListener('submit', handleFormSubmit);
-
 function closePopupForm() {
   popup.classList.remove('popup_opened');
-  popupForm.reset();
-  inputTitle.placeholder = profileName.textContent;
-  inputProfession.placeholder = profileProfession.textContent;
 }
 
 function handleFormSubmit (evt) {
@@ -36,3 +31,7 @@ function handleFormSubmit (evt) {
 
   closePopupForm();
 }
+
+popupClose.addEventListener('click', closePopupForm);
+
+popupForm.addEventListener('submit', handleFormSubmit);
